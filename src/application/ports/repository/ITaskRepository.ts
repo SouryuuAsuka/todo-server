1492c48870a8401e317e1526d58c751d0ac02db0
Asuka <asuka@>
2023-10-09 19:13:54 +0300

    edit task fix 2

--- src/application/ports/repository/ITaskRepository.ts
+++ src/application/ports/repository/ITaskRepository.ts
@@ -2,5 +2,5 @@ import { Task } from "@domain/interfaces";
 
 export interface ITaskRepository {
   create(projectId: number, userId: number, task: Task): Promise<boolean>;
-  edit(taskId: number, projectId: number, task: Task): Promise<any[]>;
+  edit(taskId: number, projectId: number, task: Task): Promise<boolean>;
 }
