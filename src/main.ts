326d153640dea89934db28a87f9069be7ffe6138
Asuka <asuka@>
2023-10-10 04:36:17 +0300

    file upload add

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import 'module-alias/register';
 import * as express from 'express';
 import { projectRouter, taskRouter, commentRouter, userRouter } from '@framework/web/routes';
 import dependency from '@configuration/projectDependencies';
+import * as fs from 'fs';
 
 const PORT = 3000;
 const HOST = '0.0.0.0';
@@ -15,3 +16,11 @@ app.use('/api/users/', userRouter(dependency));
 app.use('/api/projects/', projectRouter(dependency));
 app.use('/api/projects/', taskRouter(dependency));
 app.use('/api/projects/', commentRouter(dependency));
+app.post('/api/files', (req, res) => {
+  fs.writeFile("../dist", req.body.file, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+}); 
+})
